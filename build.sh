002fa827982db0fff1ea558bdd7ae3b4020b8d78
test-user <test@email.com>
2020-07-22 00:34:07 +0900

    2020-07-21 spark24 hotspot

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ fi
 
 # ref: https://blog.kkty.jp/entry/2019/06/16/214951
 tar -czh . | docker build \
-        -t ${IMAGE_TAG:-jupyter_datascience_spark} \
+        -t ${IMAGE_TAG:-jupyter_datascience_spark24} \
         --build-arg BASE_IMAGE=${BASE_IMAGE:-adoptopenjdk:8u252-b09-jre-hotspot-bionic} \
         -
