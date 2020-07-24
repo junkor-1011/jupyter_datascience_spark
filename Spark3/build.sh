429d9fcdbd17feaf1c9e8e95310c005f984bb3d9
junkor-1011 <tomoyukizxy@gmail.com>
2020-07-24 14:32:33 +0900

    matplotlib3.3.0 & Spark3.0.0 with Hadoop3.2
    Squashed commit of the following:
    
    commit 5972ec349380eaf7fe964f4229287faba63b5025
    Author: junkor-1011 <tomoyukizxy@gmail.com>
    Date:   Fri Jul 24 14:20:17 2020 +0900
    
        0.0.7 (adoptopenjdk) matplotlib3.3.* Spark3.0.0 with Hadoop3.2
    
    commit aef5bb23fb7647995e19df27eeab7631e4cda9f2
    Author: junkor-1011 <tomoyukizxy@gmail.com>
    Date:   Fri Jul 24 12:17:20 2020 +0900
    
        add comment-outed line for zulu-bulkd
    
    commit 2e35faff67690d39721989d0a3ccd75c4ac0aa7c
    Author: junkor-1011 <tomoyukizxy@gmail.com>
    Date:   Fri Jul 24 03:12:12 2020 +0900
    
        update postgresql jdbc-ver (0.0.7-rc2)
    
    commit 82c40d221003f3930789c3552f7eb2d3c8780279
    Author: junkor-1011 <tomoyukizxy@gmail.com>
    Date:   Fri Jul 24 02:50:34 2020 +0900
    
        check Docker-build pass(local) & set default Spark2
    
    commit b6d5eab7abf65bafcb232314c6366c721da5bdfb
    Author: junkor-1011 <tomoyukizxy@gmail.com>
    Date:   Fri Jul 24 01:49:35 2020 +0900
    
        candidate v0.0.7
    
    commit 27692b8c2d1349a785f0a03f8a51b6323cde740a
    Author: junkor-1011 <tomoyukizxy@gmail.com>
    Date:   Fri Jul 24 01:35:31 2020 +0900
    
        WIP: update packages_freeze
    
    commit 12a70dfa4757c128f95f23196e557bdf120ebb5e
    Author: junkor-1011 <tomoyukizxy@gmail.com>
    Date:   Fri Jul 24 00:50:35 2020 +0900
    
        WIP: separate Spark2/3
    
    commit 8b029644690fefd219e96055893b9d2984c2224f
    Author: junkor-1011 <tomoyukizxy@gmail.com>
    Date:   Fri Jul 24 00:44:48 2020 +0900
    
        WIP: separate Spark2/3
    
    commit b793ff95854087943d345cb87889faeb78c5dc85
    Author: test-user <test@email.com>
    Date:   Thu Jul 23 22:47:30 2020 +0900
    
        rst to md

---
+++ Spark3/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR
+
+if [ -f $SCRIPT_DIR/.env ]; then
+    . $SCRIPT_DIR/.env
+    cat $SCRIPT_DIR/.env
+fi
+
+# ref: https://blog.kkty.jp/entry/2019/06/16/214951
+tar -czh . | docker build \
+        -t ${IMAGE_TAG:-jupyter_datascience_spark3} \
+        --build-arg CONDA_YAML=${CONDA_YAML:-'./conda_packages.yml'} \
+        --build-arg BASE_IMAGE=${BASE_IMAGE:-adoptopenjdk:8u262-b10-jre-hotspot-bionic} \
+        -
